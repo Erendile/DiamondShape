903bfd8bfebced3d4957c94eda5075091a622410
Erendile <74563063+Erendile@users.noreply.github.com>
2021-02-17 10:47:31 +0300

    Update diamondShape.c

--- diamondShape.c
+++ diamondShape.c
@@ -9,7 +9,7 @@ int main()
     {
         if(k == abs((SIZE / 2) - i))
         {
-            for(int j = 0; j < (2 * abs(abs(l) - (SIZE / 2))) + 1; j++)
+            for(int j = 0; j < 2 * abs(abs(l) - (SIZE / 2)) + 1; j++) // 2x + 1 -> 1, 3, 5, 7, 9...
                 printf("*");
 
             printf("\n"), i++, k = -1, l--;
